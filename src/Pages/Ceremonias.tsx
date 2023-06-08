18d8ef9bd909536472aa0bcc25c093757a3ec313
carlos <carlosm1795@gmail.com>
2023-06-08 11:03:46 -0600

    Initial Files

---
+++ src/Pages/Ceremonias.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Ceremonias = () => {
+  return (
+    <div>Ceremonias asd asd asdas asd as</div>
+  )
+}
+
+export default Ceremonias
---
+++ src/Pages/Iniciaciones.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Iniciaciones = () => {
+  return (
+    <div>Iniciaciones asd as dasdas dasd asd asd a</div>
+  )
+}
+
+export default Iniciaciones
